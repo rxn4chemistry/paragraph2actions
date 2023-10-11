ae83b36f38e271d8eeaafc517d2f7edd51117fca
Alain Vaucher <ava@zurich.ibm.com>
2023-10-11 10:41:12 +0200

    update copyright notice

--- src/paragraph2actions/__init__.py
+++ src/paragraph2actions/__init__.py
@@ -1,10 +1,3 @@
 """Package initialization."""
 
-__copyright__ = """
-LICENSED INTERNAL CODE. PROPERTY OF IBM.
-IBM Research Licensed Internal Code
-(C) Copyright IBM Corp. 2023
-ALL RIGHTS RESERVED
-"""
-
 __version__ = "1.1.0"  # managed by bump2version
