4c068801637e97c3d8261bfe58ffb9db1139da75
Alain Vaucher <avaucher@users.noreply.github.com>
2023-10-13 13:00:02 +0200

    Download CDE model automatically if needed (#19)

--- src/paragraph2actions/sentence_splitting/cde_splitter.py
+++ src/paragraph2actions/sentence_splitting/cde_splitter.py
@@ -1,8 +1,13 @@
+import logging
 from typing import List
 
 import chemdataextractor
+from chemdataextractor.data import Package
 
 from .sentence_splitter import SentenceSplitter, SentenceSplittingError
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 class CdeSplitter(SentenceSplitter):
@@ -12,6 +17,7 @@ class CdeSplitter(SentenceSplitter):
 
     def __init__(self, split_sentences_at_newlines: bool = True):
         super().__init__(split_sentences_at_newlines=split_sentences_at_newlines)
+        download_cde_data()
 
     def _split_impl(self, text: str) -> List[str]:
         try:
@@ -19,3 +25,14 @@ class CdeSplitter(SentenceSplitter):
             return [sentence.text for sentence in paragraph.sentences]
         except Exception as e:
             raise SentenceSplittingError(text) from e
+
+
+def download_cde_data() -> None:
+    """Explicitly download the CDE model necessary for splitting sentences, if needed."""
+    package = Package("models/punkt_chem-1.0.pickle")
+    if package.local_exists():
+        return
+
+    logger.info("Downloading the necessary ChemDataExtractor data...")
+    package.download()
+    logger.info("Downloading the necessary ChemDataExtractor data... Done.")
